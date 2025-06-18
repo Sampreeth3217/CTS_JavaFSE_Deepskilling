3f285d63c7bcb5fac34ace7c15e332fc120586fd
Sampreeth Chowdary Nimmagadda <nschowdary1234@gmail.com>
2025-06-18 23:49:18 +0530

    Updated Week 2 exercises

--- Week_1/Exercise_1/Logger.java
+++ Week_1/Exercise_1/Logger.java
@@ -1,20 +1,17 @@
 public class Logger {
     private static Logger instance;
-
     // Private constructor to prevent instantiation
-    private Logger() {
+    private Logger(){
         System.out.println("Logger initialized.");
     }
-
     // Public method to provide access to the instance
     public static Logger getInstance() {
-        if (instance == null) {
-            instance = new Logger();
+        if (instance==null) {
+            instance=new Logger();
         }
         return instance;
     }
-
-    public void log(String message) {
-        System.out.println("LOG: " + message);
+    public void log(String message){
+        System.out.println("LOG: "+message);
     }
 }
